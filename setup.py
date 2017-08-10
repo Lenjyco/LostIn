df7ef58e9fa3c98395678b0d189cccef2cc71a83
Piednoir Jocelyn <piednoir.jocelyn@gmail.com>
2017-08-10 11:10:34 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='LostIn',
+      version='1.0',
+      description='Python French Depute Spammer',
+      author='Laos',
+      install_requires=['requests', 'tweepy'])
